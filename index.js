264f0cb944877b8e8ececee89063f8fb010163b7
amanparab1 <amanparab007@gmail.com>
2025-12-17 18:50:05 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello from devops-pipelines!");
+console.log("This is a test script for GitHub Actions.");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Test script running...");
+setTimeout(() => {
+    console.log("Test script completed after 3 seconds");
+}, 3000);
+console.log("Starting tests ...");
